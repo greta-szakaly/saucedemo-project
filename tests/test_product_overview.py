08b7d90e2db2b8e3269964c94e7f4f1c23f2b70b
greta-szakaly <sz.greta97@gmail.com>
2026-04-21 11:45:26 +0200

    Restructure product related test scopes and test scripts

--- tests/test_product_overview.py
+++ tests/test_product_overview.py
@@ -1,6 +1,5 @@
 import pytest
 from playwright.sync_api import Page, expect
-import json
 
 EXPECTED_PRODUCTS = [
     "Sauce Labs Backpack",
@@ -51,88 +50,3 @@ def test_all_product_names_are_clickable(authenticated_page):
         expect(authenticated_page.get_by_role("button", name="Back to products")).to_be_visible()
 
         authenticated_page.goto("https://www.saucedemo.com/inventory.html")
-        
-
-def test_product_information_is_consistent(authenticated_page):
-    products = authenticated_page.get_by_test_id("inventory-item")
-
-    for product in products.all():
-        productName = product.get_by_test_id("inventory-item-name").text_content()
-        productDescription = product.get_by_test_id("inventory-item-desc").text_content()
-        productPrice = product.get_by_test_id("inventory-item-price").text_content()
-        
-        product.get_by_role("img").click()
-
-        expect(authenticated_page.get_by_test_id("inventory-item-name")).to_have_text(productName)
-        expect(authenticated_page.get_by_test_id("inventory-item-desc")).to_have_text(productDescription)
-        expect(authenticated_page.get_by_test_id("inventory-item-price")).to_have_text(productPrice)
-
-        authenticated_page.goto("https://www.saucedemo.com/inventory.html")
-
-def test_back_to_products_navigation(authenticated_page):
-    authenticated_page.get_by_test_id("inventory-item-name").nth(0).click()
-
-    backToProductsButton = authenticated_page.get_by_role("button", name="Back to products")
-    expect(backToProductsButton).to_be_enabled()
-
-    backToProductsButton.click()
-    expect(authenticated_page).to_have_url("https://www.saucedemo.com/inventory.html")
-
-def test_add_one_product_to_cart_from_overview(authenticated_page):
-    cartBadge = authenticated_page.get_by_test_id("shopping-cart-badge")
-    #addedProductAmount = cartBadge.text_content()
-
-    firstProduct = authenticated_page.get_by_test_id("inventory-item").nth(0)
-    firstProductAddToCartButton = firstProduct.get_by_role("button", name="Add to cart")
-    firstProductRemoveButton = firstProduct.get_by_role("button", name="Remove")
-
-    expect(firstProductAddToCartButton).to_be_enabled()
-    
-    firstProductAddToCartButton.click()
-    #addedProductAmount += 1
-    
-    expect(firstProductRemoveButton).to_be_enabled()
-    expect(cartBadge).to_have_text("1")
-
-def test_add_one_product_to_cart_from_detail(authenticated_page):
-    addToCartButton = authenticated_page.get_by_role("button", name="Add to cart")
-    removeButton = authenticated_page.get_by_role("button", name="Remove")
-    cartBadge = authenticated_page.get_by_test_id("shopping-cart-badge")
-
-    authenticated_page.get_by_test_id("inventory-item-name").nth(0).click()
-    expect(addToCartButton).to_be_enabled()
-
-    addToCartButton.click()
-    expect(cartBadge).to_have_text("1")
-    expect(removeButton).to_be_enabled()
-    
-def test_add_multiple_products_to_cart_from_overview(authenticated_page):
-    cartBadge = authenticated_page.get_by_test_id("shopping-cart-badge")
-    products = authenticated_page.get_by_test_id("inventory-item")
-
-    for product in products.all():
-        product.get_by_role("button", name="Add to cart").click()
-        expect(product.get_by_role("button", name="Remove")).to_be_enabled()
-
-    expect(cartBadge).to_have_text("6")
-
-def test_remove_one_product_from_cart_from_overview(authenticated_page):
-    authenticated_page.evaluate("localStorage.setItem('cart-contents', '[0]')")
-    authenticated_page.reload()
-
-    cartBadge = authenticated_page.get_by_test_id("shopping-cart-badge")
-    expect(cartBadge).to_have_text("1")
-
-    authenticated_page.get_by_role("button", name="Remove").click()
-    expect(cartBadge).to_have_count(0) # expect this element to not exist
-
-
-def test_remove_one_product_from_cart_from_detail(authenticated_page):
-    authenticated_page.evaluate("localStorage.setItem('cart-contents', '[0]')")
-    authenticated_page.goto("https://www.saucedemo.com/inventory-item.html?id=0")
-
-    cartBadge = authenticated_page.get_by_test_id("shopping-cart-badge")
-    expect(cartBadge).to_have_text("1")
-
-    authenticated_page.get_by_role("button", name="Remove").click()
-    expect(cartBadge).to_have_count(0) # expect this element to not exist
--- testscopes/test_product_overview.md
+++ testscopes/test_product_overview.md
@@ -1,18 +1,9 @@
-# Test scope for products
+# Test scope for product overview
 ## Product list
 1. Product list is visible
 2. All products are present
-
-## Product detail
-1. Product image is clickable
-2. Product name is clickable
-3. Product information is consistent
-4. Back to products navigation
-
-## Add to cart functionality
-1. Add one product to cart
-2. Add multiple products to cart
-3. Remove one product from cart
+3. All product images are clickable
+4. All product names are clickable
 
 ## Sorting functionality
 1. Sort by Name (A to Z)
